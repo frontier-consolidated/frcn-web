32c4074ea66613ba3d4f37fd8f493c451150094f
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-01-19 21:30:38 +0000

    Configure FRCN primary colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,18 +12,31 @@ const config = {
 	theme: {
 		extend: {
 			colors: {
-				// flowbite-svelte
+				// colors: https://flowbite-svelte.com/docs/pages/colors
+				// primary: {
+				// 	50: "#f8fafc",
+				// 	100: "#f1f5f9",
+				// 	200: "#e2e8f0",
+				// 	300: "#cbd5e1",
+				// 	400: "#94a3b8",
+				// 	500: "#64748b",
+				// 	600: "#475569",
+				// 	700: "#334155",
+				// 	800: "#1e293b",
+				// 	900: "#0f172a",
+				// },
+
 				primary: {
-					50: "#FFF5F2",
-					100: "#FFF1EE",
-					200: "#FFE4DE",
-					300: "#FFD5CC",
-					400: "#FFBCAD",
-					500: "#FE795D",
-					600: "#EF562F",
-					700: "#EB4F27",
-					800: "#CC4522",
-					900: "#A5371B",
+					50: "#EAF4FF",
+					100: "#C3DDF7",
+					200: "#A5CAEF",
+					300: "#8BBDEF",
+					400: "#5BA5EF",
+					500: "#3492EF",
+					600: "#1784F2",
+					700: "#1471CE",
+					800: "#105396",
+					900: "#0A3E70",
 				},
 			},
 		},
