cae43efc0e4e431cd8d8f5bfa1012d1b3bee4a7d
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-01-19 21:37:19 +0000

    i18n setup

---
+++ src/lib/i18n/index.ts
@@ -0,0 +1,11 @@
+import { browser } from "$app/environment";
+import { init } from "svelte-i18n";
+
+const defaultLocale = "en";
+
+// register("en", () => import("./lang/en.json"))
+
+init({
+	fallbackLocale: defaultLocale,
+	initialLocale: browser ? window.navigator.language : defaultLocale,
+});
