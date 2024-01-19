428c0fafeb0048136280355e4ffad62789340ae2
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-01-19 21:44:04 +0000

    NavUser component

---
+++ src/lib/components/NavUser.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import { Avatar, Dropdown, DropdownItem, DropdownDivider, Toggle } from "flowbite-svelte";
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<div class="flex items-center space-x-2 cursor-pointer">
+	<span class="text-md font-semibold">SyntheticDev</span>
+	<Avatar rounded size="sm" />
+</div>
+<Dropdown class="w-44">
+	<DropdownItem>My Profile</DropdownItem>
+	<DropdownItem>Settings</DropdownItem>
+	<DropdownDivider />
+	<li>
+		<Toggle class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">Streamer Mode</Toggle>
+	</li>
+	<DropdownItem
+		slot="footer"
+		on:click={() => {
+			dispatch("logout");
+		}}>Logout</DropdownItem
+	>
+</Dropdown>
