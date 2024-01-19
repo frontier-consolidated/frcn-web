b6a45c87bad436b875fcf1cf3d0863805dbcd40f
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-01-19 21:45:32 +0000

    Exclude markdown dependencies for optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		exclude: [
+			"codemirror",
+			"@codemirror/view",
+			"@codemirror/state",
+			"@codemirror/lang-markdown",
+			"@uiw/codemirror-theme-github",
+		],
+	},
 });
