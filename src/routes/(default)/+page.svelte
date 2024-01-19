036f06329c7e38c23c1d6257b0772a7958acd815
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-01-19 21:36:05 +0000

    Create home page

---
+++ src/routes/(default)/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Alert } from "flowbite-svelte";
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div class="p-8">
+	<Alert color="red">
+		<span class="font-medium">Info alert!</span>
+		Change a few things up and try submitting again.
+	</Alert>
+</div>
