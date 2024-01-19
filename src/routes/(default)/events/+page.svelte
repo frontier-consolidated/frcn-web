5cf207befe1eb489fba1a610cdde96226bd1cf11
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-01-19 21:36:41 +0000

    Create events page

---
+++ src/routes/(default)/events/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Button } from "flowbite-svelte";
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
+</script>
+
+<div class="flex justify-end">
+	<Button href="/event/asdjkasd">Create New Event</Button>
+</div>
