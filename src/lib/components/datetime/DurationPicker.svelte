27b0e33237a3e367153722f958b7b4be3013b801
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-01-19 21:40:45 +0000

    Duration picker

---
+++ src/lib/components/datetime/DurationPicker.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { Input, Dropdown, DropdownHeader } from "flowbite-svelte";
+	import { ClockSolid } from "flowbite-svelte-icons";
+	import DurationControls from "./duration/DurationControls.svelte";
+	import { toDuration } from "./helpers";
+
+	export let id: string;
+	export let title: string | undefined = undefined;
+	export let value: number | null = null;
+
+	let dropdownOpen = false;
+
+	let inputValue: string = "";
+	$: {
+		if (inputValue && (!value || inputValue != toDuration(value))) {
+			let duration = Number(inputValue);
+			if (isNaN(duration)) {
+				inputValue = toDuration(value ?? 0);
+			} else {
+				if (duration < 1000) duration *= 1000;
+
+				value = duration;
+				inputValue = toDuration(duration);
+			}
+		} else if (value) {
+			inputValue = toDuration(value);
+		}
+	}
+</script>
+
+<Input bind:value={inputValue} {id} placeholder="Select duration" {...$$restProps}>
+	<ClockSolid slot="left" size="sm" class="ms-1" />
+</Input>
+<Dropdown
+	bind:open={dropdownOpen}
+	class="border rounded-lg border-gray-300 dark:border-gray-600 p-4"
+>
+	{#if title}
+		<DropdownHeader>
+			{title}
+		</DropdownHeader>
+	{/if}
+	<DurationControls bind:value />
+</Dropdown>
