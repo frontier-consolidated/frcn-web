5b806c5817921fbaa5009d3e3eaee6e876108250
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-01-19 21:43:46 +0000

    Tooltip component

---
+++ src/lib/components/Tooltip.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Popover } from "flowbite-svelte";
+	import { InfoCircleSolid } from "flowbite-svelte-icons";
+
+	export let id: string;
+	export let size: "xs" | "sm" | "md" | "lg" | "xl" = "md";
+	export let placement: "left" | "right" = "right";
+</script>
+
+<InfoCircleSolid {id} {size} class={placement == "right" ? "ms-2" : "me-2"} />
+<Popover triggeredBy="#{id}" {...$$restProps}>
+	<slot />
+</Popover>
