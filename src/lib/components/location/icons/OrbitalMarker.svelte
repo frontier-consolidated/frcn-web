e6178f9720e6127686ca404296d6c97d98ae769e
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-01-19 21:42:02 +0000

    location icons

---
+++ src/lib/components/location/icons/OrbitalMarker.svelte
@@ -0,0 +1,26 @@
+<svg
+	width="512"
+	height="512"
+	viewBox="0 0 512 512"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	{...$$restProps}
+>
+	<g clip-path="url(#clip0_2_42)">
+		<rect
+			x="6.94113"
+			y="255.74"
+			width="351.855"
+			height="351.855"
+			rx="40"
+			transform="rotate(-45 6.94113 255.74)"
+			stroke="currentcolor"
+			stroke-width="48"
+		/>
+	</g>
+	<defs>
+		<clipPath id="clip0_2_42">
+			<rect width="512" height="512" fill="currentcolor" />
+		</clipPath>
+	</defs>
+</svg>
---
+++ src/lib/components/location/icons/Planet.svelte
@@ -0,0 +1,10 @@
+<svg
+	width="512"
+	height="512"
+	viewBox="0 0 512 512"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	{...$$restProps}
+>
+	<circle cx="256" cy="256" r="221" stroke="currentcolor" stroke-width="48" />
+</svg>
