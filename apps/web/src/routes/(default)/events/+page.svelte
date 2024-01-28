4cce49ab22400969cbcd2aeedefc3b388ebebbc1
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-01-28 01:02:15 +0000

    Update events page

--- apps/web/src/routes/(default)/events/+page.svelte
+++ apps/web/src/routes/(default)/events/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
-	import { Button, Card, Heading, TabItem, Tabs, Timeline, TimelineItem } from "flowbite-svelte";
-	import { CirclePlusSolid } from "flowbite-svelte-icons";
+	import { strings } from "@frcn/shared";
+	import { Breadcrumb, BreadcrumbItem, Button, Card, Heading, Pagination, TabItem, Tabs, Timeline, TimelineItem } from "flowbite-svelte";
+	import { CalendarMonthSolid, CirclePlusSolid } from "flowbite-svelte-icons";
 
 	import TimeBadge from "$lib/components/datetime/TimeBadge.svelte";
+	import LocationBreadcrumbItem from "$lib/components/location/LocationBreadcrumbItem.svelte";
 	import { Mutations, apollo } from "$lib/graphql";
 	import { pushNotification } from "$lib/stores/NotificationStore";
 
@@ -44,36 +46,62 @@
 
 <Tabs contentClass="p-4" style="underline">
 	<TabItem title="Timeline" open>
-		<Timeline>
-			{#each data.events as event}
-				<TimelineItem>
-					<div class="mb-2">
-						<TimeBadge id="test-event-time" format="datetime-relative" value={event.startAt ?? 0} />
-					</div>
-					<Card
-						img={event.imageUrl ?? undefined}
-						horizontal
-						class="w-full !max-w-none"
-						padding="none"
-						href="/event/{event.id}"
-					>
-						<div class="flex flex-col gap-2 px-4 py-2">
-							<span class="text-xl font-semibold dark:text-white">
-								{event.name}
-							</span>
-							<div>
-								<span class="block text-md font-semibold dark:text-white">
-									Summary
-								</span>
-								<span class="dark:text-gray-400">
-									{event.summary}
-								</span>
-							</div>
+		<div class="flex flex-col items-center">
+			<Timeline class="w-full">
+				{#each data.events as event}
+					<TimelineItem>
+						<div class="mb-2">
+							<TimeBadge id="test-event-time" format="datetime-relative" value={event.startAt ?? 0} />
 						</div>
-					</Card>
-				</TimelineItem>
-			{/each}
-		</Timeline>
+						<Card
+							img={event.imageUrl ?? undefined}
+							horizontal
+							class="w-full !max-w-none"
+							padding="none"
+							href="/event/{event.id}"
+						>
+							<div class="flex flex-col gap-2 px-4 py-2">
+								<span class="text-xl font-semibold dark:text-white">
+									{event.name}
+									<Breadcrumb
+										ariaLabel="Event Type and Location"
+										olClass="inline-flex flex-wrap items-center space-x-1 md:space-x-3 rtl:space-x-reverse"
+									>
+										{#if event.eventType}
+											<BreadcrumbItem home>
+												<svelte:fragment slot="icon">
+													<CalendarMonthSolid class="w-4 h-4 me-2" />
+												</svelte:fragment>
+												{strings.toTitleCase(event.eventType)} Event
+											</BreadcrumbItem>
+										{/if}
+										{#if event.location}
+											{#each event.location as item}
+												<LocationBreadcrumbItem location={item} />
+											{/each}
+										{:else}
+											<BreadcrumbItem>???</BreadcrumbItem>
+										{/if}
+									</Breadcrumb>
+								</span>
+								<div>
+									<span class="block text-md font-semibold dark:text-white">
+										Summary
+									</span>
+									<span class="dark:text-gray-400">
+										{event.summary}
+									</span>
+								</div>
+							</div>
+						</Card>
+					</TimelineItem>
+				{/each}
+			</Timeline>
+			<Pagination pages={[
+				{ name: `${data.page + 1}`, href: `/events?page=${data.page}` },
+				{ name: `${data.page + 2}`, href: `/events?page=${data.page}` }
+			]} />
+		</div>
 	</TabItem>
 	<TabItem title="Calendar">
 		
--- apps/web/src/routes/(default)/events/+page.ts
+++ apps/web/src/routes/(default)/events/+page.ts
@@ -1,25 +1,44 @@
+import { browser } from "$app/environment";
 import { getLocations } from "@frcn/shared/locations";
 
 import { Queries, apollo } from "$lib/graphql";
+import { pushNotification } from "$lib/stores/NotificationStore";
 
 import type { PageLoad } from "./$types";
 
 
 export const load = (async () => {
-	const { data } = await apollo.query({
-		query: Queries.GET_EVENTS,
-	});
+    try {
+        const { data } = await apollo.query({
+            query: Queries.GET_EVENTS,
+        });
+    
+        const events = (data.events?.items ?? []).map(event => ({
+            ...event,
+            location: event.location ? getLocations(event.location) : null
+        }))
+    
+        return {
+            events,
+            page: data.events?.page ?? 0,
+            nextPage: data.events?.nextPage,
+            prevPage: data.events?.prevPage,
+            total: data.events?.total ?? 0
+        };
+    } catch (err) {
+        if (browser) {
+            pushNotification({
+                type: "error",
+                message: "Error connecting to server"
+            })
+        }
 
-    const events = (data.events?.items ?? []).map(event => ({
-        ...event,
-        location: getLocations(event.location)
-    }))
-
-	return {
-        events,
-        page: data.events?.page ?? 0,
-        nextPage: data.events?.nextPage,
-        prevPage: data.events?.prevPage,
-        total: data.events?.total ?? 0
-	};
+        return {
+            events: [],
+            page: 0,
+            nextPage: null,
+            prevPage: null,
+            total: 0
+        }
+    }
 }) satisfies PageLoad;
