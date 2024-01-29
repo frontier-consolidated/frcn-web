e36bd23650bd2583bf70fdbc8692587889231e38
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-01-29 14:13:51 +0000

    Improve index page

--- apps/web/src/routes/(default)/+page.svelte
+++ apps/web/src/routes/(default)/+page.svelte
@@ -1,8 +1,53 @@
 <script lang="ts">
+	import { Carousel, Img } from "flowbite-svelte";
+	import type { HTMLImgAttributes } from "svelte/elements";
+
+	import calendarImage from "$lib/images/stock/calendar.png"
+	import image1 from "$lib/images/stock/carousel/1.png"
+	import image2 from "$lib/images/stock/carousel/2.png"
+	import image3 from "$lib/images/stock/carousel/3.png"
+	import image4 from "$lib/images/stock/carousel/4.png"
+	import eventsImage from "$lib/images/stock/events.png"
+
+	import PageCard from "./PageCard.svelte";
+
+	const images: HTMLImgAttributes[] = [
+		{
+			src: image1,
+		},
+		{
+			src: image2,
+		},
+		{
+			src: image3,
+		},
+		{
+			src: image4,
+		}
+	]
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Frontier Consolidated Events</title>
+	<meta name="description" content="Get involved with the latest community events" />
+	<link rel="preload" href={eventsImage} as="image" />
 </svelte:head>
 
+<Carousel {images} duration={7000} class="h-72 sm:h-80 md:h-96" let:Controls let:Indicators>
+	<Controls />
+	<Indicators  />
+</Carousel>
+<div class="grid min-[450px]:grid-cols-2 mt-4 gap-4">
+	<PageCard
+		src={eventsImage}
+		alt="Group of players gathered in a circle"
+		href="/events"
+		caption="Upcoming Events"
+	/>
+	<PageCard
+		src={calendarImage}
+		alt="A ship flying over a planet towards the sun"
+		href="/events#calendar"
+		caption="Events Calendar"
+	/>
+</div>
---
+++ apps/web/src/routes/(default)/PageCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Img } from "flowbite-svelte";
+
+    export let src: string;
+    export let href: string;
+    export let alt: string | undefined = undefined;
+    export let caption: string;
+</script>
+<a {href}>
+    <Img
+        {src}
+        {alt}
+        class="rounded-lg"
+        imgClass="h-full w-full object-cover"
+        figClass="relative h-48 sm:h-64 cursor-pointer transition duration-300 filter hover:grayscale"
+        captionClass="absolute w-full bottom-0 p-2 sm:p-4 bg-black bg-opacity-70 text-white font-semibold text-md"
+        {caption}
+    />
+</a>
