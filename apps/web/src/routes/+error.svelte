1feff2d80f9af737c1ac8af32fbdeb7f83e154af
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-02-04 12:21:47 +0000

    Add custom 401 page

--- apps/web/src/routes/+error.svelte
+++ apps/web/src/routes/+error.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import { page } from "$app/stores";
-	import { Alert } from "flowbite-svelte";
+	import { Alert, Heading } from "flowbite-svelte";
+
+	import Hr from "$lib/components/Hr.svelte";
 
 	let title = "";
-	switch ($page.status) {
+	$: switch ($page.status) {
 		case 404:
 			title = "Page Not Found";
+			break;
+		case 401:
+			title = "Unauthenticated"
 			break;
 		case 500:
 			title = "Server Error";
@@ -17,11 +22,24 @@
 </script>
 
 <svelte:head>
-	<title>{title}</title>
+	<title>{$page.status} - {title}</title>
+	<meta name="robots" content="noindex" />
 </svelte:head>
 
 <main class="flex flex-1 flex-col p-4 w-full max-w-5xl my-0 mx-auto box-border">
-	<Alert color="red">
-		<span>{$page.status}: {$page.error?.message}</span>
-	</Alert>
+	{#if $page.status === 401}
+		<div class="my-auto py-16">
+			<div class="flex flex-col items-center rounded-lg p-6 bg-gray-900 border-2 border-red-800 border-dashed">
+				<Heading tag="h1" class="font-medium text-4xl text-center">Access Denied</Heading>
+				<p class="text-gray-500">{$page.status}</p>
+				<Hr class="w-full" />
+				<p class="mt-4 text-gray-400">You must be authenticated in order to view this page</p>
+
+			</div>
+		</div>
+	{:else}
+		<Alert color="red">
+			<span>{$page.status}: {$page.error?.message}</span>
+		</Alert>
+	{/if}
 </main>
