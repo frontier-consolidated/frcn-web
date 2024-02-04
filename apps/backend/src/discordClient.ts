567271787b27c4a009fc34a58c5021feb3b20bc1
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-02-04 21:33:53 +0000

    Update discordClient.ts

--- apps/backend/src/discordClient.ts
+++ apps/backend/src/discordClient.ts
@@ -2,8 +2,8 @@ import { Client, GatewayIntentBits, REST } from "discord.js";
 
 import { load } from "./bot";
 
-export function createDiscordClient() {
-	const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+export function createDiscordClient(token: string) {
+	const rest = new REST({ version: "10" }).setToken(token);
 	const client = new Client({
 		intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages],
 	});
