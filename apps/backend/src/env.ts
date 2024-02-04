3c1b31681b882b989037a71409c80ac4e4e752c0
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-02-04 16:10:59 +0000

    Add validation method for environment

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -1,3 +1,5 @@
+import { assert } from "console";
+
 export function getPort() {
 	return Number(process.env.PORT);
 }
@@ -49,4 +51,69 @@ export function getOrigins() {
 
 export function isProd() {
 	return process.env.NODE_ENV === "production"
+}
+
+export function validateEnvironment() {
+	console.log("Validating environment...")
+
+	// PORT=
+	assert(!isNaN(getPort()), "Environment: Expected 'PORT' to be a number")
+
+	// EXTERNAL_PORT=
+	assert(!isNaN(getExternalPort()), "Environment: Expected 'EXTERNAL_PORT' to be a number")
+
+	// DOMAIN=
+	assert(process.env.DOMAIN && process.env.DOMAIN.length > 0, "Environment: Expected 'DOMAIN'")
+	
+	// SUB_DOMAIN=
+	// xxx
+
+	// WEB_ORIGIN=
+	assert(process.env.WEB_ORIGIN && process.env.WEB_ORIGIN.length > 0, "Environment: Expected 'WEB_ORIGIN'")
+
+	// ORIGINS=
+	assert(process.env.ORIGINS && process.env.ORIGINS.length > 0, "Environment: Expected 'ORIGINS'")
+
+	// DATABASE_URL=
+	assert(process.env.DATABASE_URL && process.env.DATABASE_URL.length > 0, "Environment: Expected 'DATABASE_URL'")
+
+	// CONSENT_COOKIE=
+	assert(process.env.CONSENT_COOKIE && process.env.CONSENT_COOKIE.length > 0, "Environment: Expected 'CONSENT_COOKIE'")
+
+	// SESSION_COOKIE=
+	assert(process.env.SESSION_COOKIE && process.env.SESSION_COOKIE.length > 0, "Environment: Expected 'SESSION_COOKIE'")
+
+	// SESSION_SECRET=
+	assert(process.env.SESSION_SECRET && process.env.SESSION_SECRET.length > 0, "Environment: Expected 'SESSION_SECRET'")
+
+	// DEVICE_TRACK_COOKIE=
+	assert(process.env.DEVICE_TRACK_COOKIE && process.env.DEVICE_TRACK_COOKIE.length > 0, "Environment: Expected 'DEVICE_TRACK_COOKIE'")
+
+	// ADMIN_DISCORD_ID=
+	if (isProd()) {
+		assert(process.env.ADMIN_DISCORD_ID && process.env.ADMIN_DISCORD_ID.length > 0, "Environment: Expected 'ADMIN_DISCORD_ID' in production")
+	}
+
+	// DISCORD_CLIENTID=
+	assert(process.env.DISCORD_CLIENTID && process.env.DISCORD_CLIENTID.length > 0, "Environment: Expected 'DISCORD_CLIENTID'")
+
+	// DISCORD_SECRET=
+	assert(process.env.DISCORD_SECRET && process.env.DISCORD_SECRET.length > 0, "Environment: Expected 'DISCORD_SECRET'")
+
+	// DISCORD_TOKEN=
+	assert(process.env.DISCORD_TOKEN && process.env.DISCORD_TOKEN.length > 0, "Environment: Expected 'DISCORD_TOKEN'")
+
+	// AWS_S3_BUCKET=
+	assert(process.env.AWS_S3_BUCKET && process.env.AWS_S3_BUCKET.length > 0, "Environment: Expected 'AWS_S3_BUCKET'")
+
+	// AWS_S3_REGION=
+	assert(process.env.AWS_S3_REGION && process.env.AWS_S3_REGION.length > 0, "Environment: Expected 'AWS_S3_REGION'")
+
+	// AWS_S3_KEY=
+	assert(process.env.AWS_S3_KEY && process.env.AWS_S3_KEY.length > 0, "Environment: Expected 'AWS_S3_KEY'")
+
+	// AWS_S3_SECRET=
+	assert(process.env.AWS_S3_SECRET && process.env.AWS_S3_SECRET.length > 0, "Environment: Expected 'AWS_S3_SECRET'")
+
+	console.log("No issues with environment found")
 }
