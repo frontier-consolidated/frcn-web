c589b999f13b95acbbd76f468e681233f47dae1c
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-02-16 11:48:10 +0000

    Create UserRoleSelectDropdown.svelte

---
+++ apps/web/src/lib/components/UserRoleSelectDropdown.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Dropdown } from "flowbite-svelte";
+
+	import type { RoleFragmentFragment } from "$lib/graphql/__generated__/graphql";
+
+    export let filter: (role: RoleFragmentFragment) => boolean = () => true;
+</script>
+
+<Dropdown>
+
+</Dropdown>
