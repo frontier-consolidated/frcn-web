9354d971c2f2a8274b68fa27e28d82eedcf40789
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-02-16 11:48:02 +0000

    Move role fetch to top-level layout for use in user modal

--- apps/web/src/routes/+layout.ts
+++ apps/web/src/routes/+layout.ts
@@ -2,9 +2,10 @@ import { browser } from "$app/environment";
 import "$lib/i18n"; // Import to initialize. Important :)
 import { locale, waitLocale } from "svelte-i18n";
 
-export const load = async () => {
+export const load = async ({ data }) => {
 	if (browser) {
 		locale.set(window.navigator.language);
 	}
 	await waitLocale();
+	return data;
 };
