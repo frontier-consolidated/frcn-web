d0e3ca79a201496f61dd660857d89b3ded28359f
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-18 22:39:34 +0000

    Console.error encoded login errors

--- apps/web/src/routes/+layout.ts
+++ apps/web/src/routes/+layout.ts
@@ -4,9 +4,22 @@ import { locale, waitLocale } from "svelte-i18n";
 import "$lib/i18n"; // Import to initialize. Important :)
 import { integration } from "$lib/integration.js";
 
-export const load = async ({ data }) => {
+function base64URLdecode(str: string) {
+	const base64Encoded = str.replace(/-/g, '+').replace(/_/g, '/');
+	const padding = str.length % 4 === 0 ? '' : '='.repeat(4 - (str.length % 4));
+	const base64WithPadding = base64Encoded + padding;
+	return atob(base64WithPadding).split('').map(char => String.fromCharCode(char.charCodeAt(0))).join("");
+}
+
+export const load = async ({ data, url }) => {
 	if (browser) {
 		locale.set(window.navigator.language);
+		if (url.searchParams.has("login_err")) {
+			const encodedError = url.searchParams.get("login_err")!
+			const decodedError = base64URLdecode(encodedError)
+			const err = JSON.parse(decodedError)
+			console.error(err)
+		}
 	}
 	await waitLocale();
 
