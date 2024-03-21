e880870fdf61b8a068c01fdc52d3909223da9bba
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-21 16:28:05 +0000

    Fix envs

--- apps/web/src/cms.server.ts
+++ apps/web/src/cms.server.ts
@@ -1,9 +1,12 @@
+
 import { CMSEvents } from "@frcn/cms-events"
 import { createEventHandler, createTBus } from "pg-tbus"
 
+import { CMS_BUS_SCHEMA, CMS_BUS_DATABASE_URL } from "$env/static/private"
+
 const bus = createTBus("cms", {
-    db: { connectionString: process.env.CMS_BUS_DATABASE_URL },
-    schema: process.env.CMS_BUS_SCHEMA
+    db: { connectionString: CMS_BUS_DATABASE_URL },
+    schema: CMS_BUS_SCHEMA
 })
 
 const cms_update_handler = createEventHandler({
