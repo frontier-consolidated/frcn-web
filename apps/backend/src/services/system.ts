2a78d90fc430991815da78a007d9c397269b5c3b
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-22 16:26:05 +0000

    Fix key implementation

--- apps/backend/src/services/system.ts
+++ apps/backend/src/services/system.ts
@@ -14,16 +14,37 @@ async function getSystemSettings() {
 	});
 }
 
+function encodeKey(id: number, uuid: string) {
+	return Buffer.from(`${id}.${uuid}`, "utf-8").toString("base64url")
+}
+
+function decodeKey(key: string) {
+	const [idString, uuid] = Buffer.from(key, "base64url").toString("utf-8").split(".")
+	if (!idString || !uuid) return null;
+
+	const id = Number(idString)
+	if (isNaN(id)) return null;
+
+	return {id, uuid}
+}
+
 async function getAccessKey(key: string) {
-	const hash = await bcrypt.hash(key, ACCESS_KEY_SALT_ROUNDS)
-	return await database.accessKey.findUnique({
-		where: { hashedKey: hash }
+	const keyData = decodeKey(key)
+	if (!keyData) return null;
+
+	const accessKey = await database.accessKey.findUnique({
+		where: { id: keyData.id }
 	})
+	if (!accessKey) return null;
+
+	const match = await bcrypt.compare(keyData.uuid, accessKey.hashedKey)
+	if (!match) return null;
+	return accessKey;
 }
 
 async function createAccessKey() {
-	const key = randomUUID()
-	const hash = await bcrypt.hash(key, ACCESS_KEY_SALT_ROUNDS)
+	const uuid = randomUUID()
+	const hash = await bcrypt.hash(uuid, ACCESS_KEY_SALT_ROUNDS)
 
 	const accessKey = await database.accessKey.create({
 		data: {
@@ -32,7 +53,7 @@ async function createAccessKey() {
 			permissions: 0
 		}
 	})
-	return [accessKey, key] as const;
+	return [accessKey, encodeKey(accessKey.id, uuid)] as const;
 }
 
 async function regenerateAccessKey(id: number) {
@@ -46,7 +67,7 @@ async function regenerateAccessKey(id: number) {
 		}
 	})
 
-	return key
+	return encodeKey(id, key)
 }
 
 export const $system = {
