b167fef075ddb1e276ce6a7a43d6b0f8fe8790a4
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-23 15:23:47 +0000

    Update cms.server.ts

--- apps/web/src/cms.server.ts
+++ apps/web/src/cms.server.ts
@@ -4,19 +4,25 @@ import { createEventHandler, createTBus } from "pg-tbus"
 
 import { CMS_BUS_SCHEMA, CMS_BUS_DATABASE_URL } from "$env/static/private"
 
-const bus = createTBus("cms", {
-    db: { connectionString: CMS_BUS_DATABASE_URL },
-    schema: CMS_BUS_SCHEMA
-})
+async function setupCmsEventHandlers() {
+    const bus = createTBus("cms", {
+        db: { connectionString: CMS_BUS_DATABASE_URL },
+        schema: CMS_BUS_SCHEMA
+    })
+    
+    const cms_update_handler = createEventHandler({
+        task_name: "handle_cms_update",
+        eventDef: CMSEvents.cms_update_event,
+        handler: async (props) => {
+            console.log("CMS UPDATE", props)
+        }
+    })
+    
+    bus.registerHandler(cms_update_handler)
+    
+    await bus.start()
+}
 
-const cms_update_handler = createEventHandler({
-    task_name: "handle_cms_update",
-    eventDef: CMSEvents.cms_update_event,
-    handler: async (props) => {
-        console.log("CMS UPDATE", props)
-    }
-})
-
-bus.registerHandler(cms_update_handler)
-
-await bus.start()
+if (import.meta.env.PROD) {
+    await setupCmsEventHandlers()
+}
