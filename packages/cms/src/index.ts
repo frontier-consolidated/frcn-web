3656f5924aa10eabda5e9310ab2a63a91a51e23f
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-23 16:48:28 +0000

    Move events to separate folder and export

--- packages/cms/src/index.ts
+++ packages/cms/src/index.ts
@@ -1,14 +1,1 @@
-import pgtbus from "pg-tbus";
-import type * as pgtbusExports from "pg-tbus";
-const { defineEvent, Type } = pgtbus as unknown as typeof pgtbusExports
-
 export * from "./containers"
-
-const cms_update_event = defineEvent({
-    event_name: "cms_update",
-    schema: Type.String()
-})
-
-export const CMSEvents = {
-    cms_update_event
-}
