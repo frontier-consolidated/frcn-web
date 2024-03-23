6d0386e7a33b54032c28d306e6c6ab0a6d1182e8
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-23 22:42:21 +0000

    Add mbtc and disclaimer to Footer

--- apps/web/src/lib/components/Footer.svelte
+++ apps/web/src/lib/components/Footer.svelte
@@ -4,6 +4,7 @@
 
 	import { DISCORD_URL, GITHUB_URL, YOUTUBE_URL } from "$lib/constants";
     import logo from "$lib/images/logo.png";
+    import madeByTheCommunity from "$lib/images/mbtc.png"
 	import { cookieConsentModal } from "$lib/stores/CookieConsentModalStore";
 
 	import Hr from "./Hr.svelte";
@@ -14,10 +15,13 @@
 <Footer footerType="socialmedia" class="relative flex flex-col items-center bg-gray-200 dark:bg-gray-900 mt-12">
     <div class="absolute -top-[1.9rem] right-0 clip-tl-8 w-[40%] h-8 bg-gray-200 dark:bg-gray-900"></div>
     <div class="w-full max-w-6xl md:flex md:justify-between mt-4 gap-8">
-        <div class="shrink-0 mb-6 md:mb-0">
+        <div class="flex md:flex-col shrink-0 mb-6 md:mb-0">
             <ScreenQuery size="sm" let:matches>
                 <FooterBrand href="/" src={logo} alt="Frontier Consolidated Logo" name={matches ? "Frontier Consolidated" : "FRCN"} classImg="drop-shadow" classSpan="text-gray-700" />
             </ScreenQuery>
+            <div class="flex-1 md:flex-none flex justify-end md:justify-center md:mt-4">
+                <img src={madeByTheCommunity} alt="Made by the Star Citizen community" class="aspect-square w-16" />
+            </div>
         </div>
         <div class="grid min-[380px]:grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
@@ -53,8 +57,11 @@
         </div>
     </div>
     <Hr class="w-full my-8 bg-gray-300" />
-    <div class="w-full max-w-6xl sm:flex sm:items-center sm:justify-between">
-        <FooterCopyright href="/" by="Frontier Consolidated" />
+    <div class="w-full max-w-6xl sm:flex sm:items-center sm:justify-between sm:gap-4">
+        <div class="max-w-xl">
+            <FooterCopyright href="/" by="Frontier Consolidated" spanClass="block text-sm text-gray-500 dark:text-gray-400" />
+            <span class="block text-xs text-gray-500 dark:text-gray-400">This is an unofficial Star Citizen site, not affiliated with the Cloud Imperium group of companies. All content on this site not authored by its host or users are property of their respective owners.</span>
+        </div>
         <div class="relative flex mt-4 space-x-6 rtl:space-x-reverse sm:justify-center sm:mt-0 pr-16">
             <FooterIcon href={YOUTUBE_URL}>
                 <YoutubeSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" tabindex="-1" />
