2bb9c42038a75171e8fdd64f11040214a7e0e06f
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-24 22:35:02 +0000

    Update SectionContainer.ts

--- packages/cms/src/containers/classes/SectionContainer.ts
+++ packages/cms/src/containers/classes/SectionContainer.ts
@@ -12,4 +12,13 @@ export class SectionContainer extends CmsContainer {
     override getAllowedChildren() {
         return [CMSContainerType.Section, CMSContainerType.CallToAction]
     }
+
+    clone(): SectionContainer {
+        return new SectionContainer({
+            id: this.id,
+            identifier: this.identifier,
+            title: this.title,
+            content: this.content,
+        })
+    }
 }
