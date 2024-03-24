0e70a8d29213cb42201951323dcd7963862ee3b4
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-24 17:27:33 +0000

    Update +page.server.ts

--- apps/web/src/routes/cms/+page.server.ts
+++ apps/web/src/routes/cms/+page.server.ts
@@ -1,12 +1,15 @@
+
 import { cmsClient } from '$lib/server/cms';
 
 import type { PageServerLoad } from './$types';
 
 export const prerender = false
 
-export const load = (async () => {
+export const load = (async ({ depends }) => {
+    depends("cms:indexes")
+
     const indexes = await cmsClient.getIndexes()
-    
+
     return {
         indexes
     };
