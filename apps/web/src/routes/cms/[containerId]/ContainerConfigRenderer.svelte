911dff301f6b9fd6e92bd7c5a177a7a824b94698
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-26 16:55:04 +0000

    Implement gallery container config

--- apps/web/src/routes/cms/[containerId]/ContainerConfigRenderer.svelte
+++ apps/web/src/routes/cms/[containerId]/ContainerConfigRenderer.svelte
@@ -6,6 +6,7 @@
 
     import AboutSectionContainerConfig from "./AboutSectionContainerConfig.svelte";
 	import CtaContainerConfig from "./CtaContainerConfig.svelte";
+	import GalleryContainerConfig from "./GalleryContainerConfig.svelte";
 	import IndexContainerConfig from "./IndexContainerConfig.svelte";
 	import SectionContainerConfig from "./SectionContainerConfig.svelte";
     
@@ -20,6 +21,8 @@
     <AboutSectionContainerConfig {validator} {isChild} bind:container_={container} />
 {:else if container.type === CMSContainerType.Section}
     <SectionContainerConfig {validator} {isChild} bind:container_={container} />
+{:else if container.type === CMSContainerType.Gallery}
+    <GalleryContainerConfig {validator} {isChild} bind:container_={container} />
 {:else if container.type === CMSContainerType.CallToAction}
     <CtaContainerConfig {validator} {isChild} bind:container_={container} />
 {:else}
