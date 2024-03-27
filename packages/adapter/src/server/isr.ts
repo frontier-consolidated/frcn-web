715d5957e93462a8a70c404227aeb3ad7fbe0892
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-27 21:59:19 +0000

    Update isr.ts

--- packages/adapter/src/server/isr.ts
+++ packages/adapter/src/server/isr.ts
@@ -14,3 +14,12 @@ export function is_prerender_valid(pathname: string) {
     if (!is_isr_rendered(pathname)) return true;
     return is_isr_valid(pathname);
 }
+
+export function set_isr_route_valid(pathname: string) {
+    isrCache.set(pathname, true)
+}
+
+export function invalidate_isr_route(pathname: string) {
+    console.log("Invalidate route", pathname)
+    isrCache.set(pathname, false)
+}
