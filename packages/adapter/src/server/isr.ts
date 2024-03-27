b2f34a2e602781d0136a8096a94ea3b4ff89dc71
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-27 22:51:08 +0000

    Update isr.ts

--- packages/adapter/src/server/isr.ts
+++ packages/adapter/src/server/isr.ts
@@ -1,3 +1,7 @@
+import fs from "fs";
+import { pipeline as pipe } from "stream/promises";
+import zlib from "zlib";
+
 import { isr } from "MANIFEST";
 
 const isrCache = new Map<string, boolean>()
@@ -22,4 +26,22 @@ export function set_isr_route_valid(pathname: string) {
 export function invalidate_isr_route(pathname: string) {
     console.log("Invalidate route", pathname)
     isrCache.set(pathname, false)
+}
+
+export async function compress_file(file: string, format: "gz" | "br" = "gz") {
+	const compress =
+		format == 'br'
+			? zlib.createBrotliCompress({
+					params: {
+						[zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
+						[zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
+						[zlib.constants.BROTLI_PARAM_SIZE_HINT]: fs.statSync(file).size
+					}
+				})
+			: zlib.createGzip({ level: zlib.constants.Z_BEST_COMPRESSION });
+
+	const source = fs.createReadStream(file);
+	const destination = fs.createWriteStream(`${file}.${format}`);
+
+	await pipe(source, compress, destination);
 }
