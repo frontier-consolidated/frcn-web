b1c6f3c7a658c1b05b321b83225401ede965acbf
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-30 09:23:37 +0000

    Add padding-top to PageHero

--- apps/web/src/lib/components/PageHero.svelte
+++ apps/web/src/lib/components/PageHero.svelte
@@ -8,7 +8,7 @@
 
 <div class="relative">
 	<img {srcset} {alt} class={twMerge("w-full object-cover min-h-[20rem] brightness-90 bg-slate-950 mask-fade-bottom", height)} />
-	<div class="absolute top-0 left-0 h-full w-full flex flex-col justify-center items-center gap-4 text-center">
+	<div class="absolute top-0 left-0 h-full w-full flex flex-col justify-center items-center gap-4 text-center pt-8">
 		<slot />
 	</div>
 </div>
