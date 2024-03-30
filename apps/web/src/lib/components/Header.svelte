6e3fef076681a34af465f7aa3b0c0291cea0a1aa
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-03-30 09:19:06 +0000

    Add descriptions to about us items

--- apps/web/src/lib/components/Header.svelte
+++ apps/web/src/lib/components/Header.svelte
@@ -36,10 +36,10 @@
 	$: activeUrl = $page.url.pathname;
 
 	const aboutItems = [
-		{ name: "COMMUNITY", href: "/about/community", description: "< Something about community >", icon: UsersSolid },
-		{ name: "ORGANISATION", href: "/about/org", description: "< Something about org >", icon: BuildingSolid },
-		{ name: "ACTIVITIES", href: "/about/activities", description: "< Something about activities >", icon: MapPinAltSolid },
-		{ name: "MERCH STORE", href: MERCH_URL, target: "_blank", description: "< Something about merch >", icon: BagSolid },
+		{ name: "COMMUNITY", href: "/about/community", description: "Try before you fly", icon: UsersSolid },
+		{ name: "ORGANISATION", href: "/about/org", description: "Be part of something great", icon: BuildingSolid },
+		{ name: "ACTIVITIES", href: "/about/activities", description: "The joy of multiplayer action", icon: MapPinAltSolid },
+		{ name: "MERCH STORE", href: MERCH_URL, target: "_blank", description: "Show your true colours", icon: BagSolid },
 	]
 
 	let aboutOpen = false;
