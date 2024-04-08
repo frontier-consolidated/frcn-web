8116c4a7555ca575b0dd9997780c8230afb8b5f9
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-04-08 18:20:13 +0100

    Add database request logging

--- apps/backend/src/database/index.ts
+++ apps/backend/src/database/index.ts
@@ -6,7 +6,26 @@ import { seed } from "./seed";
 import { logger } from "../logger";
 
 const PrismaClient = PrismaClientPkg.PrismaClient;
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+	log: [
+		{
+			emit: "event",
+			level: "query"
+		},
+		{
+			emit: "event",
+			level: "error"
+		}
+	]
+});
+
+prisma.$on("query", event => {
+	logger.log(`[DATABASE] Query to ${event.target} that took ${event.duration}ms`, { query: event.query, params: event.params });
+});
+
+prisma.$on("error", event => {
+	logger.error("[DATABASE] Database Error", event);
+});
 
 const database = prisma;
 
