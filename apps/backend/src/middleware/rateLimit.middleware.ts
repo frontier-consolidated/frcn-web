483084404d649421dc291361b2e9ac323c53a3e3
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-04-16 15:20:55 +0100

    Setup rate limit middleware

--- apps/backend/src/middleware/rateLimit.middleware.ts
+++ apps/backend/src/middleware/rateLimit.middleware.ts
@@ -1,19 +1,37 @@
 import rateLimit from "express-rate-limit";
 
+import { getBasePath } from "../env";
+
 export function rateLimitMiddleware() {
     const limiter = rateLimit({
         standardHeaders: true,
         legacyHeaders: true,
+        requestPropertyName: "rateLimit",
         
         windowMs: 60 * 1000,
-        limit(req, res) {
-            if (req.url === "/graphql") {
-                return 100;
+        limit(req) {
+            let tokenMultiplier = 1;
+            if (req.user) {
+                tokenMultiplier = 2;
+            } else if (req.accessKey) {
+                tokenMultiplier = 1.5;
             }
-            return 250;
+
+            if (req.url === getBasePath() + "/graphql") {
+                return 60 * tokenMultiplier;
+            }
+            return 250 * tokenMultiplier;
         },
 
-        
+        skip(req) {
+            return req.isLocal;
+        },
+
+        keyGenerator(req) {
+            if (req.user) return `${req.remoteIp}-${req.user.id}`;
+            if (req.accessKey) return `${req.remoteIp}-AccessKey${req.accessKey.id}`;
+            return req.remoteIp;
+        },
     });
 
     return limiter;
