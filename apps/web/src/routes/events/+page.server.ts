162d888708336658245102db17d11c5a54ae228c
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-04-16 17:42:37 +0100

    Remove redundant api calls on page load

--- apps/web/src/routes/events/+page.server.ts
+++ apps/web/src/routes/events/+page.server.ts
@@ -10,17 +10,5 @@ export const load = (async ({ locals, url, depends }) => {
         message: "Must be authenticated to view this page"
     });
 
-    try {
-        return await getEvents(locals.apollo, url);
-    } catch (err) {
-        return {
-            couldNotConnect: true,
-            events: [],
-            itemsPerPage: 1,
-            page: 0,
-            nextPage: null,
-            prevPage: null,
-            total: 0
-        };
-    }
+    return await getEvents(locals.apollo, url);
 }) satisfies PageServerLoad;
