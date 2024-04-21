0eac1eb572e6ce1fccaa497c8690213b498b9ce0
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2024-04-21 17:18:52 +0100

    Create /role command

--- apps/backend/src/bot/commands/addRole.command.ts
+++
@@ -1,7 +0,0 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
-
-export const command = new SlashCommandBuilder();
-
-export async function execute(interaction: ChatInputCommandInteraction) {
-
-}
