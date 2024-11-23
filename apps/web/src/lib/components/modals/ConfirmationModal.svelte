00b24813d63b56f4238ac48979abf408a1150fb1
Leon Semmens <35655841+Synthetic-Dev@users.noreply.github.com>
2024-11-23 18:34:08 +0000

    Swap around button positions in modals

--- apps/web/src/lib/components/modals/ConfirmationModal.svelte
+++ apps/web/src/lib/components/modals/ConfirmationModal.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
 	import { Modal } from "flowbite-svelte";
-    import { createEventDispatcher } from "svelte";
+	import { createEventDispatcher } from "svelte";
 
 	import Button from "../Button.svelte";
 
-    const dispatch = createEventDispatcher();
+	const dispatch = createEventDispatcher();
 
-    export let title: string;
-    export let open: boolean = false;
+	export let title: string;
+	export let open: boolean = false;
 </script>
 
 <Modal {title} size="xs" bind:open on:close>
-    <slot />
-    <svelte:fragment slot="footer">
-		<Button class="w-6/12" on:click={async () => {
-			dispatch("confirm");
-		}}>Confirm</Button>
-		<Button color="alternative" class="w-6/12" on:click={() => open = false}>Cancel</Button>
-  	</svelte:fragment>
+	<slot />
+	<svelte:fragment slot="footer">
+		<Button color="alternative" class="w-6/12" on:click={() => (open = false)}>Cancel</Button>
+		<Button
+			class="w-6/12"
+			on:click={async () => {
+				dispatch("confirm");
+			}}>Confirm</Button
+		>
+	</svelte:fragment>
 </Modal>
