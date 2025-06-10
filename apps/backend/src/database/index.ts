ebb1d66f721f6d47c7d55c24edc9c0077a8ff465
Leon Semmens <35655841+l3dotdev@users.noreply.github.com>
2025-06-10 13:15:06 +0100

    Remove query logging

--- apps/backend/src/database/index.ts
+++ apps/backend/src/database/index.ts
@@ -4,7 +4,6 @@ import type { ITXClientDenyList } from "@prisma/client/runtime/library";
 
 import { migrate } from "./migrate";
 import { seed } from "./seed";
-import { isProd } from "../env";
 import { logger } from "../logger";
 
 const PrismaClient = PrismaClientPkg.PrismaClient;
@@ -21,13 +20,7 @@ export const prisma = new PrismaClient({
 	]
 });
 
-if (isProd()) {
-	prisma.$on("query", event => {
-		logger.log(`[DATABASE] Query that took ${event.duration}ms`, { query: event.query, params: event.params });
-	});
-}
-
-prisma.$on("error", event => {
+prisma.$on("error", (event) => {
 	logger.error("[DATABASE] Database Error", event);
 });
 
