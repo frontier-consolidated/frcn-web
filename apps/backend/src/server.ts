2e830c54b74f33fe0ab06f2140fabda3dc02d604
Leon Semmens <35655841+l3dotdev@users.noreply.github.com>
2025-09-03 21:58:51 +0100

    Remove pid usage logging

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,11 +1,8 @@
 import { randomUUID } from "crypto";
 
-import pidusage from "pidusage";
-
 import { createApp } from "./app";
 import { initDatabase } from "./database";
 import { getDomain, getOrigin, getOrigins, getPort, validateEnvironment } from "./env";
-import { logger } from "./logger";
 import { $discord } from "./services/discord";
 import { $events } from "./services/events";
 
@@ -78,62 +75,3 @@ console.log(
 );
 
 setInterval(() => $events.$update(context).catch(console.error), $events.$UPDATE_INTERVAL);
-
-const usageThresholds = {
-	cpu: {
-		warning: 35,
-		severe: 50
-	},
-	memory: {
-		warning: 400_000_000,
-		severe: 600_000_000
-	}
-};
-
-function memFormat(memory: number) {
-	return `${Math.round((memory / 1024 / 1024) * 10) / 10}MB`;
-}
-
-setTimeout(() => {
-	// Wait a few seconds as startup uses more initial memory
-	setInterval(() => {
-		pidusage(process.pid, (err, stats) => {
-			if (err) {
-				logger.error("Error getting process metrics", err);
-				return;
-			}
-
-			if (stats.cpu > usageThresholds.cpu.severe) {
-				logger.error(logger.style("Cpu exceeded severe threshold!", { fg: "red" }), {
-					threshold: usageThresholds.cpu.severe,
-					usage: stats.cpu
-				});
-			} else if (stats.cpu > usageThresholds.cpu.warning) {
-				logger.warn(logger.style("Cpu exceeded warning threshold!", { fg: "yellow" }), {
-					threshold: usageThresholds.cpu.warning,
-					usage: stats.cpu
-				});
-			}
-
-			if (stats.memory > usageThresholds.memory.severe) {
-				logger.error(
-					logger.style(
-						`Memory usage exceeded severe threshold! Usage: ${memFormat(stats.memory)}`,
-						{ fg: "red" }
-					),
-					{ threshold: usageThresholds.memory.severe, usage: stats.memory }
-				);
-			} else if (stats.memory > usageThresholds.memory.warning) {
-				logger.warn(
-					logger.style(
-						`Memory usage exceeded warning threshold! Usage: ${memFormat(
-							stats.memory
-						)}`,
-						{ fg: "yellow" }
-					),
-					{ threshold: usageThresholds.memory.warning, usage: stats.memory }
-				);
-			}
-		});
-	}, 2000);
-}, 5000);
