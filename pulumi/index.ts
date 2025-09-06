eadbf541dcb84d566f14c500daa62074be88ac87
Leon Semmens <35655841+l3dotdev@users.noreply.github.com>
2025-09-06 13:45:46 +0100

    Update index.ts

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -3,6 +3,11 @@ import * as docker from "@pulumi/docker";
 import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
 
+const baseDomain = "frontierconsolidated.com";
+const webDomain = `${pulumi.getStack() === "prod" ? "" : `${pulumi.getStack()}.`}${baseDomain}`;
+const apiSubDomain = `${pulumi.getStack() === "prod" ? "" : `${pulumi.getStack()}-`}api`;
+const apiDomain = `${apiSubDomain}.${baseDomain}`;
+
 const appName = "frcn-web";
 const appLabels = { app: appName };
 const replicas = 1;
@@ -75,7 +80,7 @@ const namespace = new k8s.core.v1.Namespace(appName, {
 	}
 });
 
-new k8s.apps.v1.Deployment(appName, {
+const deployment = new k8s.apps.v1.Deployment(appName, {
 	metadata: {
 		namespace: namespace.metadata.name
 	},
@@ -104,21 +109,55 @@ new k8s.apps.v1.Deployment(appName, {
 							}
 						],
 						env: [
-							"DATABASE_URL",
-							"LOCAL_ACCESS_TOKEN",
-							"SESSION_SECRET",
-							"ADMIN_DISCORD_IDS",
-							"DISCORD_CLIENTID",
-							"DISCORD_SECRET",
-							"DISCORD_TOKEN",
-							"AWS_S3_BUCKET",
-							"AWS_S3_REGION",
-							"AWS_S3_KEY",
-							"AWS_S3_SECRET"
-						].map((name) => ({
-							name,
-							value: process.env[name]
-						})),
+							{
+								name: "PORT",
+								value: "3000"
+							},
+							{
+								name: "DOMAIN",
+								value: baseDomain
+							},
+							{
+								name: "SUB_DOMAIN",
+								value: apiSubDomain
+							},
+							{
+								name: "WEB_ORIGIN",
+								value: `https://${webDomain}`
+							},
+							{
+								name: "ACCESS_KEY_HEADER",
+								value: "x-frcn-access-key"
+							},
+							{
+								name: "CONSENT_COOKIE",
+								value: `frcn${pulumi.getStack() === "prod" ? "" : `-${pulumi.getStack()}`}.consent`
+							},
+							{
+								name: "DEVICE_TRACK_COOKIE",
+								value: `frcn${pulumi.getStack() === "prod" ? "" : `-${pulumi.getStack()}`}.deviceid`
+							},
+							{
+								name: "SESSION_COOKIE",
+								value: `frcn${pulumi.getStack() === "prod" ? "" : `-${pulumi.getStack()}`}.sid`
+							},
+							...[
+								"DATABASE_URL",
+								"LOCAL_ACCESS_TOKEN",
+								"SESSION_SECRET",
+								"ADMIN_DISCORD_IDS",
+								"DISCORD_CLIENTID",
+								"DISCORD_SECRET",
+								"DISCORD_TOKEN",
+								"AWS_S3_BUCKET",
+								"AWS_S3_REGION",
+								"AWS_S3_KEY",
+								"AWS_S3_SECRET"
+							].map((name) => ({
+								name,
+								value: process.env[name]
+							}))
+						],
 						readinessProbe: {
 							httpGet: {
 								path: "/health",
@@ -141,13 +180,17 @@ new k8s.apps.v1.Deployment(appName, {
 						],
 						env: [
 							{
+								name: "PORT",
+								value: "3000"
+							},
+							{
 								name: "PUBLIC_API_BASEURL",
 								value: "https://api.frontierconsolidated.com"
 							},
-							...["LOCAL_ACCESS_TOKEN"].map((name) => ({
-								name,
-								value: process.env[name]
-							}))
+							{
+								name: "LOCAL_ACCESS_TOKEN",
+								value: process.env.LOCAL_ACCESS_TOKEN
+							}
 						],
 						readinessProbe: {
 							httpGet: {
@@ -166,76 +209,77 @@ new k8s.apps.v1.Deployment(appName, {
 	}
 });
 
-// const backendService = new k8s.core.v1.Service(`${appName}-backend`, {
-// 	metadata: {
-// 		namespace: namespace.metadata.name,
-// 		name: `${appName}-backend`,
-// 		labels: appLabels
-// 	},
-// 	spec: {
-// 		ports: [{ port: 80, targetPort: "frcn-backend" }],
-// 		selector: appLabels
-// 	}
-// });
+const backendService = new k8s.core.v1.Service(`${appName}-backend`, {
+	metadata: {
+		namespace: namespace.metadata.name,
+		name: `${appName}-backend`,
+		labels: appLabels
+	},
+	spec: {
+		ports: [{ port: 80, targetPort: "frcn-backend" }],
+		selector: appLabels
+	}
+});
 
-// const websiteService = new k8s.core.v1.Service(`${appName}-website`, {
-// 	metadata: {
-// 		namespace: namespace.metadata.name,
-// 		name: `${appName}-website`,
-// 		labels: appLabels
-// 	},
-// 	spec: {
-// 		ports: [{ port: 80, targetPort: "frcn-website" }],
-// 		selector: appLabels
-// 	}
-// });
+const websiteService = new k8s.core.v1.Service(`${appName}-website`, {
+	metadata: {
+		namespace: namespace.metadata.name,
+		name: `${appName}-website`,
+		labels: appLabels
+	},
+	spec: {
+		ports: [{ port: 80, targetPort: "frcn-website" }],
+		selector: appLabels
+	}
+});
 
-// const ingress = new k8s.networking.v1.Ingress(`${appName}-ingress`, {
-// 	metadata: {
-// 		namespace: namespace.metadata.name,
-// 		name: `${appName}-ingress`,
-// 		annotations: {
-// 			"kubernetes.io/ingress.class": "nginx"
-// 		}
-// 	},
-// 	spec: {
-// 		rules: [
-// 			{
-// 				host: "api.frontierconsolidated.com",
-// 				http: {
-// 					paths: [
-// 						{
-// 							pathType: "Prefix",
-// 							path: "/",
-// 							backend: {
-// 								service: {
-// 									name: backendService.metadata.name,
-// 									port: { number: 80 }
-// 								}
-// 							}
-// 						}
-// 					]
-// 				}
-// 			},
-// 			{
-// 				host: "frontierconsolidated.com",
-// 				http: {
-// 					paths: [
-// 						{
-// 							pathType: "Prefix",
-// 							path: "/",
-// 							backend: {
-// 								service: {
-// 									name: websiteService.metadata.name,
-// 									port: { number: 80 }
-// 								}
-// 							}
-// 						}
-// 					]
-// 				}
-// 			}
-// 		]
-// 	}
-// });
+const ingress = new k8s.networking.v1.Ingress(`${appName}-ingress`, {
+	metadata: {
+		namespace: namespace.metadata.name,
+		name: `${appName}-ingress`,
+		annotations: {
+			"kubernetes.io/ingress.class": "nginx"
+		}
+	},
+	spec: {
+		rules: [
+			{
+				host: apiDomain,
+				http: {
+					paths: [
+						{
+							pathType: "Prefix",
+							path: "/",
+							backend: {
+								service: {
+									name: backendService.metadata.name,
+									port: { number: 80 }
+								}
+							}
+						}
+					]
+				}
+			},
+			{
+				host: webDomain,
+				http: {
+					paths: [
+						{
+							pathType: "Prefix",
+							path: "/",
+							backend: {
+								service: {
+									name: websiteService.metadata.name,
+									port: { number: 80 }
+								}
+							}
+						}
+					]
+				}
+			}
+		]
+	}
+});
 
-// export const ingressStatus = ingress.status;
+export const ingressStatus = ingress.status;
+export const deploymentStatus = deployment.status;
