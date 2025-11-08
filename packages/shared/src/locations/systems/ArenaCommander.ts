9d95985cb31ac1d9f76ed45612a7c51677a463fe
Kaiaaaaaaaa <kgiltun@gmail.com>
2025-11-08 01:26:37 +0100

    Title change racing

--- packages/shared/src/locations/systems/ArenaCommander.ts
+++ packages/shared/src/locations/systems/ArenaCommander.ts
@@ -3,7 +3,7 @@ import { planet, station, system, jumpPoint, moon, astroidField, outpost } from 
 export const ArenaCommander = system("Arena Commander", [
     station("Free Flight", { coordinates: { azimuthal: null, distance: null } }),
     station("Ship PVP", { coordinates: { azimuthal: null, distance: null } }),
-    station("Race", { coordinates: { azimuthal: null, distance: null } }),
+    station("Racing", { coordinates: { azimuthal: null, distance: null } }),
     station("Gun rush / FPS", { coordinates: { azimuthal: null, distance: null } }),
     station("Other / Experimental", { coordinates: { azimuthal: null, distance: null } })
 ]);
