269cc8bc4d252d3a78771eff28933892abae0487
Syila <137502522+Kaiaaaaaaaa@users.noreply.github.com>
2026-05-14 15:43:15 +0200

    Added the service stations to nyx location

--- packages/shared/src/locations/systems/Nyx.ts
+++ packages/shared/src/locations/systems/Nyx.ts
@@ -7,16 +7,16 @@ export const Nyx = system("Nyx", [
 	planet("Nyx III", { coordinates: { azimuthal: null, distance: null } }),
 
 
+	station("Levski", { coordinates: { azimuthal: null, distance: null } }),
+	station("People's Service Station ALPHA", { coordinates: { azimuthal: null, distance: null } }),
+	station("People's Service Station DELTA", { coordinates: { azimuthal: null, distance: null } }),
+	station("People's Service Station THETA", { coordinates: { azimuthal: null, distance: null } }),
+	station("People's Service Station LAMBDA", { coordinates: { azimuthal: null, distance: null } }),
 	astroidField("Nyx belt alpha", { coordinates: { azimuthal: null, distance: null } }),
-		station("Levski", { coordinates: { azimuthal: null, distance: null } }),
 	astroidField("Nyx belt beta", { coordinates: { azimuthal: null, distance: null } }),
 
 	
-	jumpPoint("Pyro Gateway", {
-		coordinates: { azimuthal: null, distance: null }
-	}),
-	jumpPoint("Stanton Gateway", {
-		coordinates: { azimuthal: null, distance: null }
-	})
+	jumpPoint("Pyro Gateway", { coordinates: { azimuthal: null, distance: null } }),
+	jumpPoint("Stanton Gateway", { coordinates: { azimuthal: null, distance: null } })
 
 ]);
